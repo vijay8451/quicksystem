eb93acc2807df790b2675c0fada39cab87788473
Vijay Singh <23010485+vijay8451@users.noreply.github.com>
2019-04-29 15:31:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-# with open('README.md', 'r', encoding='utf-8') as file:
-#     LONG_DESCRIPTION = file.read()
+with open('README.md', 'r', encoding='utf-8') as file:
+     LONG_DESCRIPTION = file.read()
 
 with open('requirements.txt') as pkg:
     required = pkg.read().splitlines()
@@ -10,8 +10,8 @@ setup(
     name='quicksystem',
     version="0.1.0",
     description="CLI tool to reserve, provision a system and use Jenkins job for Satellite.",
-    # long_description=LONG_DESCRIPTION,
-    # long_description_content_type='text/markdown',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     author="Vijay Singh",
     author_email="vijay8451@gmail.com",
     python_requires=">=3.6",
